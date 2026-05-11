8a9818ed46659c1febd3f275f8d97c7da8436464
abhipraydhoble <122669982+abhipraydhoble@users.noreply.github.com>
2026-05-11 09:38:40 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,4 +1,3 @@
-# create vpc
 resource "aws_vpc" "net" {
   cidr_block = "192.168.0.0/16"
   tags = {
@@ -6,44 +5,76 @@ resource "aws_vpc" "net" {
   }
 }
 
-# create subnet
+
 resource "aws_subnet" "pub" {
   vpc_id                  = aws_vpc.net.id
-  cidr_block              = "192.168.0.0/21"
+  cidr_block              = "192.168.0.0/22"
   availability_zone       = "ap-southeast-1a"
   map_public_ip_on_launch = true
   tags = {
-    Name = "public-subnet"
+    Name = "public subnet"
   }
 }
 
-# create internet gateway
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.net.id
   tags = {
-    Name = "igw-tf"
+    Name = "igw-vpc-tf"
   }
+
 }
 
-
-# create route table
 
 resource "aws_route_table" "rt-1" {
   vpc_id = aws_vpc.net.id
   tags = {
-    Name = "RT-Public"
+    Name = "rt-public"
   }
 
   route {
-    gateway_id = aws_internet_gateway.igw.id
     cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
+
+resource "aws_route_table_association" "rta-1" {
+  subnet_id      = aws_subnet.pub.id
+  route_table_id = aws_route_table.rt-1.id
+}
+
+
+
+resource "aws_security_group" "firewall" {
+  name   = "vpc-tf-sg"
+  vpc_id = aws_vpc.net.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
 }
 
 
-# associate subnet with rt
-resource "aws_route_table_association" "rta" {
-  subnet_id      = aws_subnet.pub.id
-  route_table_id = aws_route_table.rt-1.id
+
+resource "aws_instance" "vm" {
+  ami                    = "ami-04a8a2b994a2a7176"
+  instance_type          = "t3.micro"
+  key_name               = "id_rsa"
+  subnet_id              = aws_subnet.pub.id
+  vpc_security_group_ids = [aws_security_group.firewall.id]
+
+  tags = {
+    Name = "instance-01"
+  }
 }
